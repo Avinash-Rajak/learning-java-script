bbd1cbc8571c13431c4947e52f72cb29bcbdfbf4
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-09-16 20:19:31 +0530

    added console.log function

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
-// document.getElementById("count").innerText = 5 
+// document.getElementById("count-el").innerText = 5 
 
+// let count = 0
+// let myage = 18
 
-let count = 0
-let myage = 18
+// let firstbatch = 5
+// let secondbatch = 7
+
+// let count = firstbatch + secondbatch
+
+// let myage = 18
+// let humandogratio = 7
+
+// let mydogage = myage * humandogratio
+
+// let bonuspoint = 50
+
+// bonuspoint = bonuspoint + 50
+// bonuspoint = bonuspoint - 75
+// bonuspoint = bonuspoint + 45
+
+// let count = 5
+
+// steps 
+// initialize the count as 0
+// listen for clicks on the increment buttons
+// increment the count variables when the button is clicked
+// change the counter-el in the html to reflect the new count
+
+function increment(){
+    console.log("The button was clicked")
+}
