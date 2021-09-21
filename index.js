77fc195f9a4ce179fa908ea58ab53b5a6e025e15
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-09-21 19:43:32 +0530

    strings

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// document.getElementById("count-el").innerText = 5 
+// document.getElementById("count-el").innerText = 5
 
 // let count = 0
 // let myage = 18
@@ -21,20 +21,45 @@
 
 // let count = 5
 
-// steps 
+// steps
 // initialize the count as 0
 // listen for clicks on the increment buttons
 // increment the count variables when the button is clicked
 // change the counter-el in the html to reflect the new count
 
 let countEl = document.getElementById("count-el");
-let count = 0
-function increment(){
-    // console.log("Clicked")
-    count = count + 1
-    countEl.innerText = count
-    console.log (count)
-} 
+let count = 0;
+function increment() {
+  // console.log("Clicked")
 
+  count = count + 1;
+  // count += 1
+  countEl.innerText = count;
+  console.log(count);
+}
 
- 
+// creating a function ,(save), which logs out the count when called
+
+let saveEl = document.getElementById("save-el");
+function save() {
+    let entry = count + " - ";
+    // saveEl.innerText = entry;
+    saveEl.innerText += entry;
+    console.log(count);
+}
+
+// let username = "per"
+
+// let message = "You have 3 new notifications"
+
+// console.log (message + "," + username + "!")
+
+// let messageTouser = message + "," + username + "!"
+// console.log(messageTouser)
+
+// let myName = "Avinash"
+// let greeting = "Hi, my name is  "
+
+// let myGreeting = greeting + myName
+
+// console.log (myGreeting)
