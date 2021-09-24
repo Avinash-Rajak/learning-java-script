09ce771131fd5bee375d15a52d1d0ff0ffe80428
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-09-24 22:34:51 +0530

    practice session 1

---
+++ practice.js
@@ -0,0 +1,30 @@
+let firstName = "Avinash "
+let lastName = "Rajak"
+
+let fullName = firstName +  lastName
+
+console.log (fullName)
+
+
+let firstNam = "Avinash!"
+let message = "Hi there, "
+
+function fullMessage(){
+    console.log(message + firstNam);
+}
+
+
+let mypoint = 3
+function addpoints(){
+    mypoint += 3
+}
+
+function removepoints(){
+    mypoint -= 1
+}
+
+
+
+
+ 
+
