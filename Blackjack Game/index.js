cf8753223ea340137892b4602f0af926730458a4
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-09-24 23:49:12 +0530

    added Boolean

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -1,14 +1,20 @@
 let firstCard = 10
-let secondCard = 11
+let secondCard = 18
 
 let sum = firstCard + secondCard
 
+let cashOut = false
+let isAlive = true
 console.log (sum)
 
 if (sum < 21){
     console.log ("Dou you want to draw a new card ? 😊")
 } else if(sum === 21){
     console.log ("Woohhoo! You have a BlackJack!!!! 🎉")
+    cashOut = true
 } else if(sum > 21){
     console.log("You are out of the game 😢")
+    isAlive = false
 }
+
+console.log (cashOut)
