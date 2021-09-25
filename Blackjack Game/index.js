e5e0bd8bdbbd121363c3562669c63e930397949e
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-09-25 19:11:35 +0530

    display sum and numbers

--- Blackjack Game/index.js
+++ Blackjack Game/index.js
@@ -1,5 +1,5 @@
 let firstCard = 10
-let secondCard = 20
+let secondCard = 1
 
 let sum = firstCard + secondCard 
 
@@ -7,9 +7,17 @@ let cashOut = false
 let isAlive = true
 
 let message = ""
+
+let messageEl = document.getElementById("message-el");
+
+let sumEl = document.getElementById ("sum-el")
+ let cardsEl = document.getElementById ("cards-el")
 console.log (sum)
 
 function startGame(){
+    cardsEl.textContent = "Cards: " + firstCard + "" + secondCard;
+    sumEl.textContent = "Sum :" +  sum;
+    sum = sum+1
     if (sum < 21)
     {
         message = "Do you want to draw a new card ? 😊";
@@ -25,7 +33,9 @@ function startGame(){
         isAlive = false
     }
 
-console.log (cashOut)
-console.log (message)
+    messageEl.textContent = message
+
+// console.log (cashOut)
+
 }
 
