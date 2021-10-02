84d9cbca3b7d0e93d266c99d36d729b94cfa643e
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-10-02 20:45:57 +0530

    learning

--- Building Extensions/index.js
+++ Building Extensions/index.js
@@ -1,10 +1,13 @@
+let myLeads = ["www.google.com", "www.googl.com", "www.gogl.com", "www.gogl.co"];
 let inputBtn = document.getElementById("input-btn");
-const myLeads = []
-const inputEl = document.getElementById ("input-el")
+const inputEl = document.getElementById("input-el");
 
-inputBtn.addEventListener("click", function(){
-    console.log ("Btn clicked")
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
+});
+
+for (let myLeads = i; i = myLeads.length; i++ ){
+    console.log(myLeads);
 }
-)
-
 
