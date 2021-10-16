67f65b1f2ea4549d043a757c26881cbd652c16c0
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-10-16 19:33:26 +0530

    ...

--- responsive navbar/index.js
+++ responsive navbar/index.js
@@ -1,9 +1,20 @@
 const navSlide = () => {
-    const burger = document.querySelector(".burger")
-    const nav = document.querySelector(".nav-links")
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelector('.nav-links li') //togglr nav
 
-    burger.addEventListener('click', ()=>{
-        nav.classList.toggle('nav-active')
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
     });
+
+    //animate links
+    navLinks.forEach((link, index) => {
+        // link.style.animate = 'navLinkFade 0.5s ease forwards $(index / 7+2)s';
+        link.style.animation = 'navLinkfade 0.5s ease forwards $(index / 7 + 0.3)s';
+        // console.log(index / 7);
+    }) 
 }
+
+navSlide();
 
