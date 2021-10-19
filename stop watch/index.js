0d0c550908be74a56069fda1f0b3333087c65c20
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-10-19 19:45:05 +0530

    time started running

--- stop watch/index.js
+++ stop watch/index.js
@@ -5,6 +5,9 @@ let count = 0
 
 let timer = false
 
+let hrString = hr
+let minString = min
+let secString = sec
 
 
 function start(){
@@ -18,11 +21,52 @@ function stop(){
 
 function reset(){
     timer = false;
+     hr = 0;
+     min = 0;
+     sec = 0;
+     count = 0;
+
+    
+
+     document.getElementById("hr").innerHTML = "00" ;
+     document.getElementById("min").innerHTML = "00" ;
+     document.getElementById("sec").innerHTML =  "00";
+     document.getElementById("count").innerHTML= "00" ;
 }
 
 function stopWatch(){
     if(timer == true){
+        count = count + 1
 
+        if(count == 100){
+            sec = sec + 1;
+            count = 0
+        }
+
+        if (sec == 60) {
+          min = min + 1;
+          sec = 0;
+        }
+
+        if (min == 60){
+            hr = hr + 1
+            min = 0
+            sec = 0
+        }
+
+        if (hrString < 10){
+            document.getElementById("hr").innerHTML = "0" + hr;
+        }
+        if (minString < 10){
+            document.getElementById("min").innerHTML = "0" + min;
+        }
+        if (secString < 10){
+            document.getElementById("sec").innerHTML = "0" + sec;
+        }
+        document.getElementById("hr").innerHTML = hr;
+        document.getElementById("min").innerHTML = min;
+        document.getElementById("sec").innerHTML =  sec;
+        document.getElementById("count").innerHTML = count
 
         setTimeout("stopWatch()",10);
     }
