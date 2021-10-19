9aa05de86bd56c59d5dea46a315386b16a1182e2
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-10-19 19:11:13 +0530

    done with styling

---
+++ stop watch/index.js
@@ -0,0 +1,25 @@
+let hr = 0
+let min = 0
+let sec = 0
+let count = 0
+
+let timer = false
+
+
+
+function start(){
+    timer = true
+    stopWatch()
+}
+
+function stop(){
+
+}
+
+function reset(){
+
+}
+
+function stopWatch(){
+
+}
