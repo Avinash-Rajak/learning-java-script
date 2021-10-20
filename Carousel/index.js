5cd07c25eac5988f2f3be5797629bbfa870821e3
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-10-20 22:40:35 +0530

    EOD

---
+++ Carousel/index.js
@@ -0,0 +1,10 @@
+const carouselSlide = document.getElementById(".carousel-slide");
+const carouselImg = document.querySelector(".carousel-slide img");
+
+const prevBtn = document.querySelector("#prev");
+const nextBtn = document.querySelector("#next");
+
+let counter = 1
+const size = carouselImg[0].clientWidth;
+
+carouselSlide.style.transform = 
