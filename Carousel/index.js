750eeb64bd88b0ff213e3cb2d6d9ee8c3517e3d8
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-10-21 11:04:31 +0530

    ????

--- Carousel/index.js
+++ Carousel/index.js
@@ -1,11 +1,26 @@
-const carouselSlide = document.getElementById(".carousel-slide");
+const carouselSlide = document.querySelector(".carousel-slide");
 const carouselImg = document.querySelector(".carousel-slide img");
 
-const prevBtn = document.querySelector("#prev");
-const nextBtn = document.querySelector("#next");
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
 
 let counter = 1
 const size = carouselImg[0].clientWidth;
 
 carouselSlide.style.transform = 'translatex(' + (-size * counter)+'px)';
+
+//buttonListner
+
+// nextBtn.addEventListener('click', () => {
+//   carouselSlide.style.transition = "transfrom 0.4s ease-in-out";
+//   counter = counter + 1;
+//   console.log(counter);
+// });
+
+function next() {
+    carouselSlide.style.transition = "transfrom 0.4s ease-in-out";
+    counter++
+    console.log(counter);
+    carouselSlide.style.transform = 'translatex('+(-size*counter)+'px)'
+}
 
