cb2b4dbd569e9678ddb922a0ce2f4f07fb395ec9
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-10-21 15:59:47 +0530

    making button active

--- Carousel 2/index.js
+++ Carousel 2/index.js
@@ -1,8 +1,17 @@
-let flag = 0
+let flag = 2
 
-slideShow(0);
+function controller(x){
+    flag = flag + x
+    slideShow(flag);
+}
+
+slideShow(flag);
 function slideShow(num){
     let slides =  document.getElementsByClassName('slide');
+
+    for (y of slides){
+        y.style.display = "none"
+    }
     slides[num].style.display = "block"
 
 }
