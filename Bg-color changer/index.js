ac5f7ccd2e2788def420eec786422efd073de8f1
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-10-23 21:12:40 +0530

    done

--- Bg-color changer/index.js
+++ Bg-color changer/index.js
@@ -1,4 +1,16 @@
 function changeBG(color){
     document.body.style.backgroundColor = color
-    
+    let txt = document.getElementsByClassName("text");
+    // let txt = document.querySelector(".text .container");
+
+    if(color =="#000000"){
+       for (let elm of txt){
+           elm.style.color = "#ffffff"
+       }
+    }
+    else{
+         for (let elm of txt) {
+           elm.style.color = "#000000";
+         }
+    }
 }
