b70eb9b2381e042658a42f9e43a4ba60172d13f5
Avinash Rajak <79404579+Avinash-Rajak@users.noreply.github.com>
2021-11-12 20:10:56 +0530

    string and string template

--- Revision/index.js
+++ Revision/index.js
@@ -1,6 +1,19 @@
-alert('Hello World')
-console.error("This is an error");
+// alert('Hello World')
+// console.error("This is an error");
 
 let score;
 score = 10;
 console.log(score)
+
+let namee = "Avi"
+let age = 19
+
+// string template
+
+console.log(`My name is ${namee} and my age is ${age}`);
+
+// strings
+let s = "hello world"
+
+console.log(s.length)
+
